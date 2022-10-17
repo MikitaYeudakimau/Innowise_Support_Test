a118b9deb99d9a6578997a67eb83650ca7b01c57
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-10-17 22:28:53 +0300

    add first test, fix logout problem

--- readme.md
+++ readme.md
@@ -3,4 +3,6 @@ TODO:
 -Add fixture file for self-adding start data
 ----
 -Write tests for API
---
+----
+-~~Solve the problen with logout~~ Solved. Need to write covered test for authorization 
+---
--- src/main/tests.py
+++ src/main/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import *
 
-# Create your tests here.
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we create new account
+
+        """
+        url = "/api/auth/users/"
+        data = {
+            "email" : "test@test.by",
+            "username" : "testuser",
+            "password" : "Asdfqwe123"
+        }
+        response = self.client.post(url,data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.get(username="testuser").username,"testuser")
